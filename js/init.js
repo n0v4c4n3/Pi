3eefbc3a27ee5f659dd8b5c3dd0b9299f36bef08
Gaby RM <info@n0v4c4n3.com>
2014-04-06 17:19:58 -0300

    Commit 11

---
+++ js/init.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(iniEventos);
+
+function iniEventos() {
+    //tablita efecto
+    $(".tablita tr").hover(function () {
+        $(this).addClass("hover");
+    }, function () {
+        $(this).removeClass("hover");
+    });
+
+    //flecha arriba scroll y efecto
+    $("#arriba").on("click", function () {
+        $("html, body").animate({ scrollTop: $("#wrap").offset().top }, 300);
+    })
+}
